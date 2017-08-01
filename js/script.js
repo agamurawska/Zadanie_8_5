ef5df33917f9d8d8696864c63f16ee008d7989b6
Agnieszka <aga.murawska@gmail.com>
2017-08-01 22:29:07 +0200

    triangle field in js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+﻿// scripts.js
+
+//pole trójkąta
+
+var a = prompt ('podaj wartość dla podstawy trójkąta a'),
+	h = prompt ('podaj wartość dla wysokości h'),
+	triangleArea = a*h/2;
+
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
